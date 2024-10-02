aa5c73c840728b1f3b579143085d7e652f5c2c05
Autofilter07 <134726220+Autofilter07@users.noreply.github.com>
2024-10-02 06:20:41 +0000

    vs-code

--- BACKEND/WebSocket/index.js
+++ BACKEND/WebSocket/index.js
@@ -9,15 +9,61 @@ const io = socketIo(server);
 
 app.use(express.static("public"));
 
+var allUsersID = new Set();
+var userMap = {};
+var msgMap = {};
+
 io.on("connection", (socket) => {
   console.log("connect to socket");
+  // for user count =>
+  // const userCount = io.engine.clientCounts;
 
-  socket.on("chat", (msg) => {
-    socket.to(msg.sendId).emit("message", msg.sendMsg, msg.sendId);
-    console.log(msg.sendMsg, msg.sendId);
+  // for all users id =>
+  allUsersID.add(socket.id);
+  io.broadcast.emit("users", Array.from(allUsersID));
+
+  // map user with socket id=>
+  socket.on("register", (userId) => {
+    userMap[userId] = socket.id;
+    console.log(userId, userMap[userId], "  <=register");
+    if (msgMap[userId]) {
+      msgMap[userId].map((ele)=>
+      {
+        io.to(socket.id).emit("message",ele);
+      })
+      console.log(msgMap[userId]);
+    }
   });
 
+  
+
+  socket.on("chat", (msg) => {
+    const reciepandId = userMap[msg.sendId];
+    if (reciepandId) {
+      console.log(msg.sendId, reciepandId, "  <=send");
+      io.to(reciepandId).emit("message", msg);
+    } else {
+      if(!msgMap[msg.sendId])
+      {
+        msgMap[msg.sendId]=[];
+      }
+      msgMap[msg.sendId].push(msg);
+
+      // handleOflineUser(msg);
+      // console.log("not found");
+    }
+  });
+
+  // disconnect =>
   socket.on("disconnect", () => {
+    allUsersID.delete(socket.id);
+    for (const userId in userMap) {
+      if (userMap[userId] == socket.id) {
+        delete userMap[userId];
+        break;
+      }
+    }
+    io.broadcast.emit("users", Array.from(allUsersID));
     console.log("disconnected...");
   });
 });
